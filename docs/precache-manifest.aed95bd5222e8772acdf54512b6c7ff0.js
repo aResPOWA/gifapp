b440a462921e7657712df3384467b8a9b26f7812
Antonio <iannizzotto.ga@gmail.com>
2020-09-15 21:37:44 -0300

    docs agreado

---
+++ docs/precache-manifest.aed95bd5222e8772acdf54512b6c7ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73287241f8b1d2abe11d50aa1882dcde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db35bc8cd1c70bad7c43",
+    "url": "/static/css/main.a642a96b.chunk.css"
+  },
+  {
+    "revision": "b4b9257606c77afb0401",
+    "url": "/static/js/2.52e98bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52e98bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db35bc8cd1c70bad7c43",
+    "url": "/static/js/main.693d974d.chunk.js"
+  },
+  {
+    "revision": "698957395995ef9758f0",
+    "url": "/static/js/runtime-main.a72ba0a5.js"
+  }
+]);
